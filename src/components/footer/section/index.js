d62786d635750dd8e109196d2dfe9ea9be705559
ivo2666 <ivailocvetkov365@gmail.com>
2020-10-19 22:24:07 +0300

    footer

---
+++ src/components/footer/section/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Section = styled.div`
+width: 265px;
+    display: flex;
+    flex-direction: column;
+    margin-left: 40px;
+`
+
+export default Section
